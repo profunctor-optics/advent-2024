e813e54d4fc8755d47a5a21a0c5a10cea21d2f31
Georgi Krastev <joro.kr.21@gmail.com>
2024-12-31 23:29:54 +0200

    Solve Day 10

---
+++ src/main/scala/advent2024/Day10.scala
@@ -0,0 +1,75 @@
+package advent2024
+
+import scala.reflect.ClassTag
+
+case object Day10 extends Day:
+  type Input = IArray[Int]
+  final case class Pos(i: Int, j: Int)
+
+  trait Measure[T]:
+    def empty: T
+    def single(pos: Pos): T
+    extension (x: T)
+      def +(y: T): T
+      def measure: Int
+
+  given Measure[Set[Pos]] with
+    def empty: Set[Pos] = Set.empty
+    def single(pos: Pos): Set[Pos] = Set(pos)
+    extension (x: Set[Pos])
+      def +(y: Set[Pos]): Set[Pos] = x | y
+      def measure: Int = x.size
+
+  given Measure[Int] with
+    def empty: Int = 0
+    def single(pos: Pos): Int = 1
+    extension (x: Int)
+      def +(y: Int): Int = x + y
+      def measure: Int = x
+
+  private def trailSum[T: ClassTag](map: IArray[IArray[Int]])(using T: Measure[T]): Int =
+    if map.isEmpty then return 0
+    val n = map.length
+    val m = map.head.length
+    val positions = Array.fill(10)(List.empty[Pos])
+    val trails = Array.ofDim[T](n, m)
+
+    for
+      i <- map.indices
+      j <- map(i).indices
+    do
+      val h = map(i)(j)
+      if h < positions.length
+      then positions(h) ::= Pos(i, j)
+
+    def reachable(pos: Pos, h: Int): T =
+      if h == 9 then return T.single(pos)
+      val Pos(i, j) = pos
+      val h1 = h + 1
+      var reachable = T.empty
+      if i > 0 && map(i - 1)(j) == h1 then reachable += trails(i - 1)(j)
+      if j > 0 && map(i)(j - 1) == h1 then reachable += trails(i)(j - 1)
+      if i < n - 1 && map(i + 1)(j) == h1 then reachable += trails(i + 1)(j)
+      if j < m - 1 && map(i)(j + 1) == h1 then reachable += trails(i)(j + 1)
+      reachable
+
+    for
+      h <- positions.indices.reverse
+      p <- positions(h)
+    do trails(p.i)(p.j) = reachable(p, h)
+    positions.head.iterator.map(p => trails(p.i)(p.j).measure).sum
+
+  def parse(line: String): Parsed[Input] =
+    val heights = line.toCharArray.map: c =>
+      if c.isDigit then c.asDigit else 10
+    Right(IArray.unsafeFromArray(heights))
+
+  def part1(input: Iterator[Input]): Int =
+    trailSum[Set[Pos]](IArray.from(input))
+
+  def part2(input: Iterator[Input]): Int =
+    trailSum[Int](IArray.from(input))
+
+  def run(): Unit =
+    printPart(1)(withFile(part1))
+    printPart(2)(withFile(part2))
---
+++ src/test/scala/advent2024/Day10Test.scala
@@ -0,0 +1,115 @@
+package advent2024
+
+class Day10Test extends munit.FunSuite:
+  val bigSample = """
+    |89010123
+    |78121874
+    |87430965
+    |96549874
+    |45678903
+    |32019012
+    |01329801
+    |10456732
+    |""".stripMargin
+
+  test("calculate trail score of sample #1"):
+    val sample = """
+      |0123
+      |1234
+      |8765
+      |9876
+      |""".stripMargin
+
+    val actual = Day10.withSample(sample)(Day10.part1)
+    assertEquals(actual, 1)
+
+  test("calculate trail score of sample #2"):
+    val sample = """
+      |...0...
+      |...1...
+      |...2...
+      |6543456
+      |7.....7
+      |8.....8
+      |9.....9
+      |""".stripMargin
+
+    val actual = Day10.withSample(sample)(Day10.part1)
+    assertEquals(actual, 2)
+
+  test("calculate trail score of sample #3"):
+    val sample = """
+      |..90..9
+      |...1.98
+      |...2..7
+      |6543456
+      |765.987
+      |876....
+      |987....
+      |""".stripMargin
+
+    val actual = Day10.withSample(sample)(Day10.part1)
+    assertEquals(actual, 4)
+
+  test("calculate trail score of sample #4"):
+    val sample = """
+      |10..9..
+      |2...8..
+      |3...7..
+      |4567654
+      |...8..3
+      |...9..2
+      |.....01
+      |""".stripMargin
+
+    val actual = Day10.withSample(sample)(Day10.part1)
+    assertEquals(actual, 3)
+
+  test("calculate trail score of the big sample"):
+    val actual = Day10.withSample(bigSample)(Day10.part1)
+    assertEquals(actual, 36)
+
+  test("calculate trail rating of sample #1"):
+    val sample = """
+      |.....0.
+      |..4321.
+      |..5..2.
+      |..6543.
+      |..7..4.
+      |..8765.
+      |..9....
+      |""".stripMargin
+
+    val actual = Day10.withSample(sample)(Day10.part2)
+    assertEquals(actual, 3)
+
+  test("calculate trail rating of sample #2"):
+    val sample = """
+      |..90..9
+      |...1.98
+      |...2..7
+      |6543456
+      |765.987
+      |876....
+      |987....
+      |""".stripMargin
+
+    val actual = Day10.withSample(sample)(Day10.part2)
+    assertEquals(actual, 13)
+
+  test("calculate trail rating of sample #3"):
+    val sample = """
+      |012345
+      |123456
+      |234567
+      |345678
+      |4.6789
+      |56789.
+      |""".stripMargin
+
+    val actual = Day10.withSample(sample)(Day10.part2)
+    assertEquals(actual, 227)
+
+  test("calculate trail rating of the big sample"):
+    val actual = Day10.withSample(bigSample)(Day10.part2)
+    assertEquals(actual, 81)
